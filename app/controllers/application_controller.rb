332be5c1b84b90dcfef20978e08625960f9f6ee1
Sam Nyavor <samnyavor@gmail.com>
2016-01-04 00:24:51 +0000

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def cors_preflight_check
-    headers['Access-Control-Allow-Origin'] = 'https://action-cable.herokuapp.com'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
