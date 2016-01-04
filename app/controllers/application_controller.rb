f32cea119ea50f0ee6cd6d050bafb12e7aa48502
Sam Nyavor <samnyavor@gmail.com>
2016-01-04 00:17:09 +0000

    Cors config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :cors_preflight_check
 
   private
   def set_user(username, hex_class)
@@ -13,6 +14,13 @@ class ApplicationController < ActionController::Base
     if session[:username].nil?
       redirect_to root_path
     end
+  end
+
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = 'https://action-cable.herokuapp.com'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
 end
